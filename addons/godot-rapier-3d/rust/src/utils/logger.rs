c66274e0c000ad78d9f06c4a65c3f28945381cbc
Loki <lokimckay@gmail.com>
2025-03-30 14:10:57 +1000

    types are way too long, switch to fern

--- addons/godot-rapier-3d/rust/src/utils/logger.rs
+++ addons/godot-rapier-3d/rust/src/utils/logger.rs
@@ -2,34 +2,85 @@ use std::fmt::Write;
 use std::fs::File;
 use std::sync::Mutex;
 use tracing::field::{Field, Visit};
+use tracing::level_filters::LevelFilter;
 use tracing::{debug, Subscriber};
-use tracing_subscriber::layer::Layer;
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::registry::LookupSpan;
-use tracing_subscriber::{fmt, Registry};
+use tracing_subscriber::filter::Filtered;
+use tracing_subscriber::fmt::format::{DefaultFields, Format, Writer};
+use tracing_subscriber::fmt::{Layer, MakeWriter};
+use tracing_subscriber::layer::{Layer as LayerTrait, Layered};
+use tracing_subscriber::reload::{Handle, Layer as ReloadLayer};
+use tracing_subscriber::{filter, fmt, Registry};
+use tracing_subscriber::{prelude::*, reload};
 
 use godot::{
     classes::{Engine, Os},
     prelude::*,
 };
-use log::{Level, LevelFilter, Metadata, Record};
 
-pub fn init_logger() {
-    let log_file = match File::create("C:/Users/loki/Desktop/test/my_cool_trace.log") {
-        Ok(file) => file,
-        Err(e) => {
-            godot_error!("Failed to open log file: {}", e);
-            return;
+pub struct GR3DLogger {
+    console_handle: Option<ConsoleHandle>,
+    file_handle: Option<FileHandle>,
+}
+
+impl GR3DLogger {
+    pub fn new() -> Self {
+        GR3DLogger {
+            console_handle: None,
+            file_handle: None,
         }
-    };
+    }
 
-    let subscriber = Registry::default()
-        .with(fmt::Layer::default().with_writer(Mutex::new(log_file)))
-        .with(GodotConsoleLogger::new());
+    pub fn init(&mut self) {
+        let console_layer = GodotConsoleLogger::new().with_filter(filter::LevelFilter::OFF);
+        let (console_layer, console_handle) = reload::Layer::new(console_layer);
 
-    tracing::subscriber::set_global_default(subscriber).expect("Logger initialization failed");
+        let file_layer = fmt::Layer::default()
+            .with_writer(Mutex::new(DummyWriter))
+            .with_filter(filter::LevelFilter::OFF);
+        let (file_layer, file_handle) = reload::Layer::new(file_layer);
 
-    debug!("Hello from tracing!");
+        let subscriber = Registry::default().with(console_layer).with(file_layer);
+
+        tracing::subscriber::set_global_default(subscriber).expect("Logger initialization failed");
+
+        debug!("Hello from tracing!");
+
+        self.console_handle = Some(console_handle);
+        self.file_handle = Some(file_handle);
+    }
+
+    // pub fn set_level(&mut self, level: LogLevel) {
+
+    // let level_filter: LevelFilter = level.into();
+    // self.file_reload_handle
+    //     .as_ref()
+    //     .unwrap()
+    //     .reload_with(filter::LevelFilter::new(level_filter));
+    // }
+
+    pub fn update_log_file_path(&mut self, new_path: String) {
+        match self.file_handle.as_ref() {
+            Some(handle) => {
+                let log_file = match File::create(new_path) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        godot_error!("Failed to open log file: {}", e);
+                        return;
+                    }
+                };
+
+                let file_layer = fmt::Layer::default()
+                    .with_writer(Mutex::new(log_file))
+                    .with_filter(filter::LevelFilter::TRACE);
+                if let Err(e) = handle.reload(file_layer) {
+                    godot_error!("Failed to update_log_file_path: {:?}", e);
+                }
+            }
+            None => {
+                godot_error!("File handle is not initialized");
+            }
+        }
+    }
 }
 
 pub struct StringVisitor<'a> {
@@ -50,7 +101,7 @@ impl GodotConsoleLogger {
     }
 }
 
-impl<S: Subscriber> Layer<S> for GodotConsoleLogger {
+impl<S: Subscriber> LayerTrait<S> for GodotConsoleLogger {
     fn on_event(
         &self,
         event: &tracing::Event<'_>,
@@ -68,68 +119,6 @@ impl<S: Subscriber> Layer<S> for GodotConsoleLogger {
         }
     }
 }
-
-// impl GodotConsoleLogger {
-//     fn set_peer_id(&mut self, peer_id: i64) {
-//         self.peer_id = Some(peer_id);
-//         self.user_data_dir = Os::get_user_data_dir(&Os::singleton()).to_string();
-//         self.log_dir = format!("{}/godot-rapier-3d/logs/{}/", self.user_data_dir, peer_id);
-//         self.log_file = format!("{}log.txt", self.log_dir);
-
-//         self.create_log_dir();
-//     }
-
-//     fn create_log_dir(&self) {
-//         if self.log_dir.is_empty() {
-//             return;
-//         }
-
-//         if let Err(e) = std::fs::create_dir_all(&self.log_dir) {
-//             godot_error!("Failed to create log directory: {}", e);
-//         }
-//     }
-
-//     fn log_to_user_data_file(&self, record: &Record) {
-//         if self.log_file.is_empty() {
-//             return;
-//         }
-
-//         let mut file = match std::fs::OpenOptions::new()
-//             .create(true)
-//             .append(true)
-//             .open(&self.log_file)
-//         {
-//             Ok(file) => file,
-//             Err(e) => {
-//                 godot_error!("Failed to open log file: {}", e);
-//                 return;
-//             }
-//         };
-
-//         file.write(format!("[GR3D][{}]: {}\n", record.level(), record.args()).as_bytes())
-//             .unwrap();
-//     }
-// }
-
-// impl log::Log for GodotConsoleLogger {
-//     fn enabled(&self, metadata: &Metadata) -> bool {
-//         metadata.level() <= log::max_level()
-//     }
-
-//     fn log(&self, record: &Record) {
-//         if self.enabled(record.metadata()) {
-//             match record.level() {
-//                 Level::Error => godot_error!("[GR3D]: {}", record.args()),
-//                 Level::Warn => godot_warn!("[GR3D]: {}", record.args()),
-//                 _ => godot_print!("[GR3D][{}]: {}", record.level(), record.args()),
-//             }
-//         }
-
-//         self.log_to_user_data_file(record);
-//     }
-
-//     fn flush(&self) {}
-// }
 
 // #[derive(Debug, Clone, GodotConvert, Var, Export)]
 // #[godot(via = GString)]
@@ -160,3 +149,42 @@ impl<S: Subscriber> Layer<S> for GodotConsoleLogger {
 //         }
 //     }
 // }
+
+type ConsoleHandle = Handle<Filtered<GodotConsoleLogger, LevelFilter, Registry>, Registry>;
+
+type ReloadableConsoleLayer =
+    Layered<ReloadLayer<Filtered<GodotConsoleLogger, LevelFilter, Registry>, Registry>, Registry>;
+
+type FileHandle<T: MakeWriter> = Handle<
+    Filtered<
+        Layer<
+            Layered<Layer<Filtered<GodotConsoleLogger, LevelFilter, Registry>, Registry>, Registry>,
+            DefaultFields,
+            Format,
+            T,
+        >,
+        LevelFilter,
+        Layered<Layer<Filtered<GodotConsoleLogger, LevelFilter, Registry>, Registry>, Registry>,
+    >,
+    Layered<Layer<Filtered<GodotConsoleLogger, LevelFilter, Registry>, Registry>, Registry>,
+>;
+
+struct DummyWriter;
+
+impl std::io::Write for DummyWriter {
+    fn write(&mut self, _buf: &[u8]) -> std::io::Result<usize> {
+        Ok(0)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> MakeWriter<'a> for DummyWriter {
+    type Writer = Self;
+
+    fn make_writer(&self) -> Self {
+        DummyWriter
+    }
+}
